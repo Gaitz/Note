6cbb8d9440d25aa39a334edc5cd83af980643f20
PHC <ragnarokcute@gmail.com>
2019-02-24 19:59:26 +0800

    add style.css and common.js to all pages.

---
+++ src/common.js
@@ -0,0 +1,55 @@
+'use strict'
+
+let navElement = document.getElementById('js-nav');
+let ul = document.createElement('ul');
+let li = document.createElement('li');
+
+/*
+<nav id="js-nav" class="websiteNav">
+    <div class="others">
+      <a href="/index.html">About System</a>
+      <a href="/others/Ideas.html">Ideas</a>
+    </div>
+
+    <div class="quests">
+      <a href="/quests/Display_Quests.html">Display Quests</a>
+      <a href="/quests/Learning_Path.html">Learning Path</a>
+    </div>
+
+    <div class="records">
+      <a href="/records/Record_Books.html">Record Books</a>
+      <a href="/records/Record_Cook.html">Record Cook</a>
+      <a href="/records/Record_Workout.html">Record Workout</a>
+      <a href="/records/Record_Achievements.html">Record Achievements</a>
+      <a href="/records/Record_Questions.html">Record Questions</a>
+    </div>
+
+    <div class="toolboxes">
+      <a href="/toolboxes/Display_All.html">Display All</a>
+      <a href="/toolboxes/Display_Computer_Science.html">Display Computer Science</a>
+      <a href="/toolboxes/Display_Manner.html">Display Manner</a>
+      <a href="/toolboxes/Display_Reading_Note.html">Display Reading Note</a>
+      <a href="/toolboxes/Display_Way_of_Learning.html">Display Way of Learning</a>
+      <a href="/toolboxes/Toolbox_Anki_Personal_Note.html">Anki Personal Note</a>
+      <a href="/toolboxes/Toolbox_Finding_The_Way_to_Learn_Languages.html">Finding The Way to Learn Languages</a>
+      <a href="/toolboxes/Toolbox_HTML.html">HTML</a>
+      <a href="/toolboxes/Toolbox_History_of_Mathematics.html">History of Mathematics</a>
+      <a href="/toolboxes/Toolbox_Markdown_Language.html">Markdown Language</a>
+      <a href="/toolboxes/Toolbox_Reading_Note_Fluent_Forever.html">Reading Note Fluent Forever</a>
+      <a href="/toolboxes/Toolbox_Reading_Note_Language_Learning_with_Anki.html">Reading Note Learning with Anki</a>
+      <a href="/toolboxes/Toolbox_Time_Management.html">Time Management</a>
+      <a href="/toolboxes/Toolbox_GitHub.html">GitHub</a>
+      <a href="/toolboxes/Toolbox_CSS.html">Toolbox CSS</a>
+      <a href="/toolboxes/Toolbox_Dress.html">Toolbox Dress</a>
+      <a href="/toolboxes/Toolbox_English_Learning.html">Toolbox English Learning</a>
+      <a href="/toolboxes/Toolbox_IPA.html">Toolbox IPA</a>
+      <a href="/toolboxes/Toolbox_Learning_Method.html">Toolbox Learning Method</a>
+      <a href="/toolboxes/Toolbox_Install_Ubuntu.html">Toolbox Install Ubuntu</a>
+      <a href="/toolboxes/Toolbox_Python.html">Toolbox Python</a>
+      <a href="/toolboxes/Toolbox_Reading_Note_The_Intelligent_Investor.html">Toolbox Reading Note The Intelligent
+        Investor</a>
+      <a href="/toolboxes/Toolbox_Vim.html">Toolbox Vim</a>
+    </div>
+  </nav>
+
+*/
