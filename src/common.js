24845a4295948013e7030ec7710ef80a178dc008
PHC <ragnarokcute@gmail.com>
2019-03-10 19:27:24 +0800

    add script injectNavigator.js to all content pages

--- src/common.js
+++ src/common.js
@@ -2,11 +2,12 @@
   'use strict'
 
   function handleNavigatorButton () {
-    const navigatorMap = document.getElementById("js-navigatorMap");
+    const navigatorMap = document.getElementById("js-lightBoxContainer");
     navigatorMap.classList.remove("hide");
     navigatorMap.classList.add("show");
   }
 
+  /* main process */
   const navigatorButton = document.getElementById("js-navigatorButton");
   navigatorButton.onclick = handleNavigatorButton;
 })();
