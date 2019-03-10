ab1b6bb8527b539a9af4dd94c0374832637c387f
PHC <ragnarokcute@gmail.com>
2019-03-10 20:56:40 +0800

    test lightbox mobile 1

--- src/common.js
+++ src/common.js
@@ -7,7 +7,16 @@
     navigatorMap.classList.add("show");
   }
 
+  function handleLightBoxClick () {
+    const navigatorMap = document.getElementById("js-lightBoxContainer");
+    navigatorMap.classList.remove("show");
+    navigatorMap.classList.add("hide");
+  }
+
   /* main process */
   const navigatorButton = document.getElementById("js-navigatorButton");
   navigatorButton.onclick = handleNavigatorButton;
+
+  const lightBoxContainer = document.getElementById("js-lightBoxContainer");
+  lightBoxContainer.onclick = handleLightBoxClick;
 })();
